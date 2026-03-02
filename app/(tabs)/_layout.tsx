8f3db29a6ce3c9dcafad0d1e943b021e063ff489
NaveenMahajan <naveenmahajan12345678@gmail.com>
2026-03-02 15:56:47 +0530

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,38 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: '#9fc2e7',
+                tabBarInactiveTintColor: '#83838a',
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: '#fff',
+                    borderTopWidth: 1,
+                    borderTopColor: '#F0F0F0',
+                    height: 68,
+                    paddingTop: 5,
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+                },
+            }}
+
+        >
+            <Tabs.Screen name="index" options={{
+                tabBarIcon: ({ color, size, focused }) => <AntDesign name="home" size={size} color={focused ? 'blue' : 'black'} />
+            }} />
+            <Tabs.Screen name="cart" options={{
+                tabBarIcon: ({ color, size, focused }) => <AntDesign name="shopping-cart" size={size} color={focused ? 'blue' : 'black'} />
+            }} />
+            <Tabs.Screen name="favorites" options={{
+                tabBarIcon: ({ color, size, focused }) => <AntDesign name="heart" size={size} color={focused ? 'red' : 'black'} />
+            }} />
+            <Tabs.Screen name="profile" options={{
+                tabBarIcon: ({ color, size, focused }) => <AntDesign name="user" size={size} color={focused ? 'blue' : 'black'} />
+            }} />
+
+        </Tabs>
+    )
 }
--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function Home() {
+export default function Cart() {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Cart</Text>
         </View>
     )
 }
